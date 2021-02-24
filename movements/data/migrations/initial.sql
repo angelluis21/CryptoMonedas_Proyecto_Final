8768138bb11b27ed02c6102c8643d8946d1c909c
Angelluis21 <anyel_luis21@hotmail.com>
2021-02-24 17:16:45 +0100

    introducción migrations/initial.sql

---
+++ movements/data/migrations/initial.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "movimientos" (
+	"id"	INTEGER,
+	"fecha"	TEXT NOT NULL,
+	"hora"	TEXT NOT NULL,
+	"moneda_from"	TEXT NOT NULL,
+	"cantidad_from"	REAL NOT NULL,
+	"moneda_to"	TEXT NOT NULL,
+	"cantidad_to"	REAL NOT NULL,
+	"conversion"	REAL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
